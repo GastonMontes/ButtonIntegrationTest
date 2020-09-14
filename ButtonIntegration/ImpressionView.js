c0a407b96a34a2f6d50fd1d1b0adb385a193f7d1
Federico Resnizky <fresnizky@yahoo.com.ar>
2020-09-14 18:29:08 -0300

    Forwarded props to RNT component

--- ButtonIntegration/ImpressionView.js
+++ ButtonIntegration/ImpressionView.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {requireNativeComponent} from 'react-native';
+import {requireNativeComponent, UIManager, findNodeHandle} from 'react-native';
 
 const RNTImpressionView = requireNativeComponent('RNTImpressionView');
 
 export default class ImpressionView extends React.Component {
     configureWithDetails = (url, offerID, rate, rateFixed) => {
         UIManager.dispatchViewManagerCommand(
-            ReactNative.findNodeHandle(this),
+          findNodeHandle(this),
       UIManager.getViewManagerConfig('RNTImpressionViewManager').Commands
         .configureWithDetails,
       [url, offerID, rate, rateFixed],
@@ -14,6 +14,6 @@ export default class ImpressionView extends React.Component {
   };
 
   render() {
-    return <RNTImpressionView />;
+    return <RNTImpressionView {...this.props} />;
   }
 }
