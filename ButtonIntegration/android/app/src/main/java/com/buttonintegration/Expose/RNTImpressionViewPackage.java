d79e4f26c69cb55238ac3775c47c3657d70adef7
Gaston Montes <gaston.montes@frogslayer.com>
2020-09-25 13:30:00 -0300

    Android - Gaston - Revert package reimplemented method.

--- ButtonIntegration/android/app/src/main/java/com/buttonintegration/Expose/RNTImpressionViewPackage.java
+++ ButtonIntegration/android/app/src/main/java/com/buttonintegration/Expose/RNTImpressionViewPackage.java
@@ -6,13 +6,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class RNTImpressionViewPackage implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return Arrays.<ViewManager>asList(new RNTImpressionViewManager(reactContext));
     }
 
     @Override
