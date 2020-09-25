6d043bbaa09528041952fb459c100859137e53db
Gaston Montes <gaston.montes@frogslayer.com>
2020-09-25 12:04:11 -0300

    Android - Gaston - Delete unused imports.

--- ButtonIntegration/android/app/src/main/java/com/buttonintegration/Expose/RNTImpressionViewManager.java
+++ ButtonIntegration/android/app/src/main/java/com/buttonintegration/Expose/RNTImpressionViewManager.java
@@ -1,19 +1,11 @@
 package com.buttonintegration.Expose;
 
-import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 
-import com.buttonintegration.BuildConfig;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.usebutton.sdk.Button;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class RNTImpressionViewManager extends SimpleViewManager<RNTImpressionView> {
     public static final String REACT_CLASS = "RNTImpressionView";
