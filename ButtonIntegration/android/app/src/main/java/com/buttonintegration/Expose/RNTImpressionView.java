ec96fb2f5318212d0def2d1f462b2fe4faa87d2c
Gaston Montes <gastonmontes@hotmail.com>
2020-10-16 15:33:54 -0300

    Gaston - Android - Delete relative layout params.

--- ButtonIntegration/android/app/src/main/java/com/buttonintegration/Expose/RNTImpressionView.java
+++ ButtonIntegration/android/app/src/main/java/com/buttonintegration/Expose/RNTImpressionView.java
@@ -3,7 +3,6 @@ package com.buttonintegration.Expose;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.buttonintegration.Model.OfferDetail;
 import com.usebutton.sdk.impression.ImpressionView;
@@ -21,9 +20,7 @@ public class RNTImpressionView extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        this.impressionView.setLayoutParams(layoutParams);
-        this.addView(this.impressionView, 0);
+        this.addView(this.impressionView);
     }
 
     // Button configuration function.
